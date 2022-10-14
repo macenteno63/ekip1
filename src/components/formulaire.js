7fe67368c24c418c1b66c86237d074e81419b6bf
jopierron <Joan.PIERRON@etu.uca.fr>
2022-10-14 11:42:01 +0200

    Front Connexion

--- src/components/formulaire.js
+++ src/components/formulaire.js
@@ -27,15 +27,17 @@ function Formulaire(){
     ) : (
         <div id={"formu"}>
             <form method={"POST"}>
-                <h1>Connexion</h1>
+                <h1 class="connexion" >Se connecter</h1>
                 <label htmlFor={"mail"}></label>
                 <input type={"email"} id={"mail"} placeholder={"Email"}/>
                 <label htmlFor={"MDP"}></label>
                 <input type={"password"} id={"MDP"} placeholder={"Mot de Passe"}/>
                 <label htmlFor={"boutonSubmit"}></label>
+                <p class="mdp" onClick={() => setIsOpen(true)} >Mot de passe oublié ?</p>
                 <input type={"submit"} id={"boutonSubmit"} value={"Se connecter"}/>
-                <p onClick={() => setIsOpen(true)} >inscription</p>
             </form>
+            <a class="inscription" > Pas encore de compte ? </a>
+            <a class="inscription" onClick={() => setIsOpen(true)} > S'inscrire </a>
         </div>
     )
 }
