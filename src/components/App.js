96c8cd1597c633ab0b4b2aee35a68f9250a2d41e
Hugo PRADIER <Hugo.PRADIER2@etu.uca.fr>
2022-10-19 09:56:03 +0200

    route reinit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import '../styles/App.css';
 import Formulaire from './formulaire.js'
 import Erreur from './erreur.js'
+import Reinit from './reinitialisationMdp'
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Formulaire/>}/> # si c'est l'accueil du site alors on affiche le Formulaire
+        <Route path="/reinitialisationMdp" element={<Reinit/>}/>
         <Route path="*" element={<Erreur/>}/> # si jamais l'url est inconnu on affiche une erreur 404
       </Routes>
     </BrowserRouter>
