6edfcde98d6a7c2767b623adfae5ec2f2b9c702b
Hugo Pradier <pradier.hugo07@gmail.com>
2022-10-23 18:55:20 +0200

    redux (échange avec la base début non fonctionnel manque quelque chose avant sur la bd)

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import {applyMiddleware,createStore} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+// Dev tools pour consulter les actions sur la base 
+// Soit en passant par l'extension graphique redux dev tools ou bien directement par la console
+import {composeWithDevTools} from 'redux-devtools-extension';
+import logger from 'redux-logger'
+
+
+
+const store = createStore(
+  rootReducer, composeWithDevTools(applyMiddleware(thunk,logger))
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+// Combine tous nos reducers pour les envoyer vers le store
+import {conbineReducers} from 'redux';
+import userReducer from './user.reducer'
+export default conbineReducers({
+    userReducer,
+});
+
