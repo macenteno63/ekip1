885463bab26718eb9b5e75b0691d90f855146af6
jopierron <Joan.PIERRON@etu.uca.fr>
2022-11-03 22:20:23 +0100

    Changement page Accueil et ajout de composants

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,4 +1,4 @@
-import '../styles/pages/erreur.css'
+import '../styles/pages/accueil.css'
 import React from 'react';
 import Navigation from '../components/Navigation';
 import Groupe from '../components/Groupe';
@@ -8,6 +8,15 @@ const Accueil = () => {
     <div>
         <Navigation/>
         <Groupe/>
+        <h2>Fil de discussion :</h2>
+        <button class="button">
+            <span>Ajouter un Post</span>
+            <svg viewBox="0 0 13 10" height="20px" width="30px">
+                <path d="M1,5 L11,5"></path>
+                <path d="M6,5 V11,5"></path>
+                <path d="M6,5 V-11,5"></path>
+            </svg>
+        </button>
     </div>
     );
 };
