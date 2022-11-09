2e4753d09e3745f66e605094e0a2170a2c95cd72
Matis MAZINGUE <Matis.MAZINGUE@etu.uca.fr>
2022-11-09 09:48:50 +0100

    front

--- src/pages/Evenement.js
+++ src/pages/Evenement.js
@@ -1,11 +1,18 @@
+import '../styles/pages/evenement.css'
 import '../styles/pages/erreur.css'
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Navigation from '../components/Navigation';
 
 const Evenement = () => {
     return (
     <div>
         <Navigation/>
+
+        <NavLink style={{ textDecoration: 'none' }} to="/addevenement" className={(nav) => (nav.isActive ? "nav-active" : "")} end>
+                    <li>Add Event</li>
+                    
+        </NavLink>
     </div>
     );
 };
