c95e4a5237d18e00e31354d3008a542cfc8a3bf2
macenteno <mateo.centeno@etu.uca.fr>
2023-01-10 16:21:28 +0100

    Updates

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-// Combine tous nos reducers pour les envoyer vers le store
-// import {combineReducers} from 'redux';
-// import userReducer from './user.reducer'
-// export default combineReducers({
-//     userReducer,
-// });
-
